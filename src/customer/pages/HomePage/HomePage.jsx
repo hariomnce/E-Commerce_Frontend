495aa39daf2d44985f2894d17130d4851705819a
Hariom Sharma <hariomne@gmail.com>
2023-09-26 18:34:46 +0530

    Added carosel

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MainCarosel from '../../components/HomeCarosel/MainCarosel'
+
+function HomePage() {
+  return (
+    <div>
+      <MainCarosel/>
+
+      <div>
+        Other section
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
