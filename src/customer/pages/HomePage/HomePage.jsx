eacbf9ae29f016b4804d23255d4ef80516c0abb3
Hariom Sharma <hariomne@gmail.com>
2023-09-27 18:40:15 +0530

    HomeSectionCard & HomeSectionCarosel added

--- src/customer/pages/HomePage/HomePage.jsx
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import MainCarosel from '../../components/HomeCarosel/MainCarosel'
+import HomeSectionCarosel from '../../components/HomeSectionCarosel/HomeSectionCarosel'
 
 function HomePage() {
   return (
     <div>
       <MainCarosel/>
 
-      <div>
-        Other section
+      <div className='space-y-10 py-20'>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
+        <HomeSectionCarosel/>
       </div>
     </div>
   )
