b8f5ac77a35e89c7464b26b06e730c98974129bc
Hariom Sharma <hariomne@gmail.com>
2023-09-28 14:08:25 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import Navigation from './customer/components/Navigation/Navigation';
-import HomePage from './customer/pages/HomePage/HomePage';
+import logo from "./logo.svg";
+import "./App.css";
+import Navigation from "./customer/components/Navigation/Navigation";
+import HomePage from "./customer/pages/HomePage/HomePage";
+import Footer from "./customer/components/Footer/Footer";
 
 function App() {
   return (
     <div className="">
-      <Navigation/>
+      <Navigation />
       <div>
-        <HomePage/>
+        <HomePage />
       </div>
+      <Footer />
     </div>
   );
 }
